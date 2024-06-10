446c97363e334fd5726040995956cc3a0ef4d73b
baljinnym13 <baljinnym1318@gmail.com>
2024-06-10 12:46:49 +0800

    text

---
+++ main.js
@@ -0,0 +1,74 @@
+// let ner = "baljaa";
+// let nas = 20;
+// let gerlesen = true;
+// let job;
+// let angi = null;
+// console.log("tani ner: ", ner);
+// console.log("tani nas: ", nas);
+// console.log("tani gerleltiin baidal: ", gerlesen);
+// console.log("tani ajil ", job);
+// console.log("tani angi: ", angi);
+// bodlogo 2
+let age = 20;
+console.log("tani nas: ", age);
+// bodlogo 3
+let fristname = "baljinnym",
+  lastname = "Bymbatsogt",
+  country = "UB",
+  email = "baljinnym13@gmail.com";
+console.log("fristname: ", fristname);
+console.log("lastname: ", lastname);
+console.log("country: ", country);
+console.log("email: ", email);
+// bodlogo 4
+let r;
+let sex = null;
+let w = 11;
+let log = "ub";
+console.log("string: ", typeof log);
+console.log("number: ", typeof w);
+console.log("boolean: ", typeof r);
+console.log("null: ", typeof sex);
+// bodlogo 5
+let x = 5,
+  y = 10;
+let z = x + y;
+let q = x - y;
+let e = x * y;
+console.log("+ operotar: ", z);
+console.log("- operotar: ", q);
+console.log("* operotar: ", e);
+
+// bodlogo 6
+let a = 4,
+  b = 5,
+  c = 6;
+let p = a + b + c;
+console.log("piremitr oloh: ", p);
+// bodlogo 7
+
+// bodlogo 10
+let f = 8,
+  g = 14;
+let h = f + g;
+let j = f - g;
+let k = f * g;
+let l = f / g;
+let v = f % g;
+console.log("+ operotar: ", h);
+console.log("- operotar: ", j);
+console.log("* operotar: ", k);
+console.log("/ operotar: ", l);
+console.log("% operotar: ", v);
+let n = f + g == 25;
+console.log("+=yu: ", n);
+let u = f - g == 2;
+console.log("-=yu: ", u);
+// bodlogo 11
+let cm = 1,
+  inch = 3.5 * cm;
+
+let inch16 = 16 * inch;
+let cm7 = 7 / inch;
+console.log("inch oloh: ", cm7);
+console.log("cm oloh: ", inch16);
